02d013e34ffe78bd462c963454cb065b4d69e303
Siddhartha <sidhartha0925@gmail.com>
2026-08-22 17:57:22 +0530

    Add right angled triangle pattern

---
+++ Basics/Patterns/02-Right-Angled-Triangle.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print (int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int n;
+    cout<<"enter the no of rows: ";
+    cin>>n;
+    print(n);
+    return 0;
+}
