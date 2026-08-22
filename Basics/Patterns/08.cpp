b826d199fe601aaf9a3152c7c16ea6a6d2b88819
Siddhartha <sidhartha0925@gmail.com>
2026-08-22 21:30:35 +0530

    08

---
+++ Basics/Patterns/08.cpp
@@ -0,0 +1,36 @@
+// Problem: Print an inverted star pyramid
+// Input: n = 5
+// Output:
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+// sidhartha thota
+
+#include<bits/stdc++.h>
+using namespace std;    
+void print(int n)
+{
+    int i,j;
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(j=1;j<=(2*i)-1;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int n;
+    cout<<"enter the no of rows: ";
+    cin>>n;
+    print(n);
+    return 0;
+}
